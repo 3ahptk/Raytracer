3ecda649a116382f095c753cd6e60de5ed23d185
cwkralap <cwkralap@mtu.edu>
2015-10-27 20:08:11 -0400

    Updated makefile to remove .png's ; Generation of .png's started

--- raytracer.c
+++ raytracer.c
@@ -2,26 +2,32 @@
 #include "stb_image_write.h"
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
+#define debug 1
+#define trace if (debug) write
+
+char buffer[256];
 const unsigned int width = 800;
 const unsigned int height = 600;
 unsigned char * ImageArray;
 
 int main(int argc, char* argv[]){
   if(argc<2||argc>3){
-    fprintf (stderr, "Invalid number of arguments: Expected 1 got %d\n", argc-1);
-    exit (EXIT_FAILURE);
+    sprintf(buffer, "Invalid number of arguments: Expected 1 got %d\n", argc-1);
+  	write(2, buffer, strlen(buffer));
+    return -1;
   }
 
-  char* filename;
-
-  if(strcmp( argv[2], "reference" )){
-    filename = "reference.png";
-  }else if(strcmp( argv[2], "custom")){
-    filename = "custom.png";
+  char filename[20];
+  if(strcmp( argv[1], "reference" )){
+    strcpy (filename,"reference.png");
+  }else if(strcmp( argv[1], "custom")){
+    strcpy (filename,"custom.png");
   }else{
-    fprintf (stderr, "Invalid output arument: Expected either \"reference\" or \"custom\"; got %s\n", argv[2]);
-    exit (EXIT_FAILURE);
+    sprintf(buffer, "Invalid output arument: Expected either \"reference\" or \"custom\"; got %s\n", argv[2]);
+  	write(2, buffer, strlen(buffer));
+    return -1;
   }
 
   ImageArray = malloc(width*height*sizeof(unsigned char));
